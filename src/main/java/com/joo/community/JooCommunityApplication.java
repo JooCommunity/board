e022092213367285bddcb01df898095034dc7e29
JooWon <joocode12@gmail.com>
2024-02-02 16:25:02 +0900

    first commit

---
+++ src/main/java/com/joo/community/JooCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.joo.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joo/community/JooCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
